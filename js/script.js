ab1ee2c995a6f7656a161004778822efaeef3d62
danielkamar <kamarster@gmail.com>
2019-04-17 13:49:40 +0300

    code refactor

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-var app = angular.module('reelDb', []);
+var app = angular.module('angularDemo', []);
 
 app.controller('MoviesController', function ($scope, $http) {
     $scope.akshaya = 'Akshaya';
     
-    $http.get('https://amruthpillai.github.io/ReelDB/assets/movies.json', { cache: true })
+    $http.get('https://koitoror.github.io/AngularDemo/assets/movies.json', { cache: true })
         .then(function (res) {
             $scope.movies = res.data;
         });
